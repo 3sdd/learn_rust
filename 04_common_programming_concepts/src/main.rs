bd7e791ee81f33cd2218312eace05b5df181fb85
3sdd <56486103+3sdd@users.noreply.github.com>
2024-01-18 12:23:30 +0900

    3.2終えた

--- 04_common_programming_concepts/src/main.rs
+++ 04_common_programming_concepts/src/main.rs
@@ -1,4 +1,91 @@
+use std::io;
+
 fn main() {
+    chapter3_1();
+    chapter3_2();
+    chapter3_2_array();
+}
+
+fn chapter3_2_array(){
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
+}
+
+fn chapter3_2() {
+    let sum = 5 + 10;
+    println!("sum:{sum}");
+
+    let difference = 95.5 - 4.3;
+    println!("diff:{difference}");
+
+    let product = 4 * 30;
+    println!("product:{product}");
+
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3;
+    println!("quotient:{quotient}");
+    println!("truncated:{truncated}");
+
+    let remainder = 53 % 5;
+    println!("remainder:{remainder}");
+
+    let t = true;
+    let f: bool = false;
+    println!("t:{t}");
+    println!("f:{f}");
+
+    // char   :  single literals
+    let c = 'z';
+    let z:char ='Z';
+    let heart_eyed_cat ='😻';
+
+    println!("{c},{z},{heart_eyed_cat}");
+
+    // tuple
+    let tup: (i32,f64,u8) = (500,6.4,1);
+    let (x,y,z)=tup;
+    println!("The value of y is {y}");
+
+    let five_hundred=tup.0;
+    let six_point_four=tup.1;
+    let one=tup.2;
+
+    println!("{five_hundred},{six_point_four},{one}");
+
+    let a=[1,2,3,4,5];
+
+    let months=["January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"];
+
+    let a:[i32;5]=[1,2,3,4,5];
+
+    // let a = [3, 3, 3, 3, 3];と同じ
+    let a= [ 3;5];
+
+    let a=[1,2,3,4,5];
+    let first=a[0];
+    let second=a[1];
+    println!("{first},{second}");
+}
+
+
+fn chapter3_1() {
     // let mut x = 5;
     // println!("The value of x is: {x}");
 
@@ -23,6 +110,7 @@ fn main() {
     // number type
     let spaces = spaces.len();
 
-    let mut spaces = "  ";
-    spaces = spaces.len();
+    // error
+    // let mut spaces = "  ";
+    // spaces = spaces.len();
 }
